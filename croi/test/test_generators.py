5346f302e6515697d3539e0a87312d7bacde1d7a
dacjames <dac@dac.io>
2015-03-14 19:03:30 -0700

    cythonize and package

--- croi/test/test_generators.py
+++ croi/test/test_generators.py
@@ -1,36 +1,39 @@
 import croi
 
 
-def test_forever():
+ITERATIONS = 1000
+
+
+def test_gen_forever():
     forever_up = croi.forever()
 
     for i, x in enumerate(forever_up):
         assert i == x
-        if i >= 10:
+        if i >= ITERATIONS:
             break
 
-    assert i == 10
+    assert i == ITERATIONS
 
     forever_down = croi.forever(step=-1)
 
     for i, x in enumerate(forever_down):
         assert i == -x
-        if i >= 10:
+        if i >= ITERATIONS:
             break
 
-    assert i == 10
+    assert i == ITERATIONS
 
     forever_step = croi.forever(initial=10, step=2)
 
     for i, x in enumerate(forever_step):
         assert i * 2 + 10 == x
-        if i >= 10:
+        if i >= ITERATIONS:
             break
 
-    assert i == 10
+    assert i == ITERATIONS
 
 
-def test_build():
+def test_gen_build():
     state = [0]
 
     def factory():
@@ -39,64 +42,64 @@ def test_build():
 
     for i, x in enumerate(croi.build(factory)):
         assert i + 1 == x
-        if i >= 10:
+        if i >= ITERATIONS:
             break
 
-    assert i == 10
+    assert i == ITERATIONS
 
 
-def test_duplicate():
+def test_gen_duplicate():
 
     for i, x in enumerate(croi.duplicate(1)):
         assert x == 1
-        if i >= 10:
+        if i >= ITERATIONS:
             break
 
-    assert i == 10
+    assert i == ITERATIONS
 
 
-def test_zeros():
+def test_gen_zeros():
     for i, x in enumerate(croi.zeros()):
         assert x == 0
-        if i >= 10:
+        if i >= ITERATIONS:
             break
 
-    assert i == 10
+    assert i == ITERATIONS
 
 
-def test_ones():
+def test_gen_ones():
     for i, x in enumerate(croi.ones()):
         assert x == 1
-        if i >= 10:
+        if i >= ITERATIONS:
             break
 
-    assert i == 10
+    assert i == ITERATIONS
 
 
-def test_randoms():
+def test_gen_randoms():
     for i, x in enumerate(croi.random_ints()):
         assert 0 <= x <= 1
-        if i >= 10:
+        if i >= ITERATIONS:
             break
 
-    assert i == 10
+    assert i == ITERATIONS
 
     for i, x in enumerate(croi.random_ints(0, 10)):
         assert 0 <= x <= 10
-        if i >= 100:
+        if i >= ITERATIONS:
             break
 
-    assert i == 100
+    assert i == ITERATIONS
 
     for i, x in enumerate(croi.random_floats(-1, 1)):
         assert -1 <= x <= 1
-        if i >= 100:
+        if i >= ITERATIONS:
             break
 
-    assert i == 100
+    assert i == ITERATIONS
 
 
-def test_take():
+def test_gen_take():
     result = list(croi.take(10, croi.forever()))
     assert [0, 1, 2, 3, 4, 5, 6, 7, 8, 9] == result
 
@@ -104,7 +107,7 @@ def test_take():
     assert [0, 1, 2] == result
 
 
-def test_drop():
+def test_gen_drop():
     result = list(croi.drop(5, range(10)))
     assert [5, 6, 7, 8, 9] == result
 
@@ -112,19 +115,19 @@ def test_drop():
     assert [3, 4, 5, 6, 7, 8, 9] == result
 
 
-def test_nth():
+def test_gen_nth():
     assert 5 == croi.nth(5, croi.forever(1))
 
 
-def test_head():
+def test_gen_head():
     assert 0 == croi.head(croi.forever())
 
 
-def test_tail():
+def test_gen_tail():
     assert [1, 2, 3, 4, 5, 6, 7, 8, 9] == list(croi.tail(range(10)))
 
 
-def test_select():
+def test_gen_select():
     result = list(croi.select(1, [1, 2, 1, 4]))
     assert [1, 1] == result
 
@@ -135,7 +138,7 @@ def test_select():
     assert ['a', 'aa', 'aaa'] == result
 
 
-def test_reject():
+def test_gen_reject():
     result = list(croi.reject(1, [1, 2, 1, 4]))
     assert [2, 4] == result
 
@@ -146,7 +149,7 @@ def test_reject():
     assert ['', 'aaaa', 'aaaaa'] == result
 
 
-def test_parition():
+def test_gen_parition():
     a, b = croi.partition(1, [1, 2, 1, 4])
     assert [1, 1] == list(a)
     assert [2, 4] == list(b)
---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from distutils.core import setup
+from distutils.extension import Extension
+
+setup_args = {
+    'name': 'croi',
+    'version': '0.1.0',
+    'packages': ['croi'],
+}
+
+try:
+    from Cython.Build import cythonize
+    use_cython = True
+
+except ImportError:
+    use_cython = False
+
+
+submodules_names = [
+    'generators',
+    'decorators',
+    'reflection',
+    'collection'
+]
+
+if use_cython:
+    setup_args['ext_modules'] = cythonize([
+        os.path.join("croi", filename + ".py")
+        for filename in submodules_names
+    ])
+
+else:
+    setup_args['ext_modules'] = [
+        Extension("croi." + filename, [os.path.join("croi", filename + ".c")])
+        for filename in submodules_names
+    ]
+
+# del setup_args['ext_modules']
+
+setup(**setup_args)
